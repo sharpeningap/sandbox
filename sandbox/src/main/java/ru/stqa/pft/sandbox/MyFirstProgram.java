8af24fb6e6566511126845655ee73d7782757a20
sharpeningap <63176402+sharpeningap@users.noreply.github.com>
2020-04-15 16:56:02 -0400

    day_2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+	
+	public static void main(String[] args) {
+
+		hello("world");
+		hello("user");
+		hello("Alex");
+
+		double l = 5;
+		System.out.println("Ploshadi kvadrata so storonoi " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Ploshadi preamougolinika so storonami " + a + " and " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+	public static double area(double a, double b) {
+		return a * b;
+	}
+
+
+}
